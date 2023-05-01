365cec5710f3f581092822b6dc71bcafc91f194f
acoolguy12 <122555202+acoolguy12@users.noreply.github.com>
2023-05-01 11:51:06 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,7 +15,7 @@ let x = 0
 joystickbit.initJoystickBit()
 comment.comment("CHANGE THE RADIO GROUP TO YOUR TEAM'S JOYSTICK NUMBER!")
 comment.comment("DO NOT CHANGE ANYTHING ELSE IN THIS CODE")
-radio.setGroup(26)
+radio.setGroup(25)
 basic.showIcon(IconNames.Yes)
 basic.showString("T")
 basic.forever(function () {
